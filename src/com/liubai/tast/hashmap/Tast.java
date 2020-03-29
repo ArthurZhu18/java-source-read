6ad400578a563a91ecdda2774ecb829e9616e715
ArthurZhu18 <1141832273@qq.com>
2020-03-29 09:23:59 +0800

    Java源码阅读项目

---
+++ src/com/liubai/tast/hashmap/Tast.java
@@ -0,0 +1,25 @@
+package com.liubai.tast.hashmap;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @ClassName Tast
+ * @Description TODO
+ * @Author ArthurZhu
+ * @Version 1.0
+ */
+public class Tast {
+
+    public static void main(String[] args) {
+
+        Map<String, Double> hashmap = new HashMap<>();
+
+        hashmap.put("K1",0.1);
+        hashmap.put("K2",0.2);
+        hashmap.put("K3",0.3);
+        hashmap.put("K4",0.4);
+
+    }
+
+}
